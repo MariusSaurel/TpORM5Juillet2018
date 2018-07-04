8fae567bc249591e0b38e2ceca9a7c056acc73fa
Marius Saurel <marius.saurel@hotmail.fr>
2018-07-04 11:24:39 +0200

    first commit

---
+++ orm_final-master/src/main/java/net/joastbg/sampleapp/entities/CompteBancaire.java
@@ -0,0 +1,19 @@
+package net.joastbg.sampleapp.entities;
+
+
+
+
+public class CompteBancaire {
+
+
+        public void setIban (String Iban)
+    {
+        
+    }
+
+        public void setProprietaire (String proprietaire)
+        {
+            
+        }
+        
+}
